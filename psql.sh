144e2b78d45e79b16be156a1cbd70780946ea561
Ros McMahon <rosmcmahon@gmail.com>
2022-12-15 11:00:16 +0700

    fix: package, start scripts

--- psql.sh
+++ psql.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-docker exec -it shepherd-pgdb-1 psql -U postgres arblacklist
-docker exec -it shepherd_pgdb_1 psql -U postgres arblacklist
+docker exec -it shepherd-db-seed-pgdb-seeder-1 psql -U postgres arblacklist
+docker exec -it shepherd_db_seed_pgdb_seeder_1 psql -U postgres arblacklist
 
